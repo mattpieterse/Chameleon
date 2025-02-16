b33d67d7f4d713b887fa111fe0aae52b0d9c4b72
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-02-17 00:18:01 +0200

    feat(script): add function to verify and parse the installation of git

--- chameleon.ps1
+++ chameleon.ps1
@@ -4,3 +4,31 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.  See LICENSE for more details.
+
+# -- Functions
+
+function Get-GitInstallation {
+    [CmdletBinding()]
+    param()
+
+    try {
+        $gitVersion = git --version
+        return @{
+            IsInstalled = $true
+            Version = $gitVersion
+        }
+    }
+    catch {
+        return @{
+            IsInstalled = $false
+            Version = $null
+        }
+    }
+}
+
+# -- Execution
+
+$gitInstallation = Get-GitInstallation
+if (-not($gitInstallation.IsInstalled)) {
+    throw("Git is not installed or not in PATH")
+}
