2a2878cc66e09b61fc3ff723149e1e845bf82bc4
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-05-16 16:51:02 +0200

    feat(config): create file based configuration manager

--- Terminal/App.cs
+++ Terminal/App.cs
@@ -1,7 +1,33 @@
-﻿namespace Terminal;
+﻿using Spectre.Console;
+using Terminal.Domain.Config;
+using Terminal.Services;
 
-public static class App
+namespace Terminal;
+
+internal static class App
 {
-    public static void Main(string[] args) =>
-        Console.WriteLine("Hello, World!");
+#region Fields
+
+    private static Config? _appConfiguration;
+
+#endregion
+
+#region Lifecycle
+
+    private static void Main(string[] args) {
+        var config = new FileConfigurationManager("config.json");
+        try {
+            _appConfiguration = config.Load();
+        }
+        catch (ArgumentException) {
+            AnsiConsole.MarkupLine($"[red]Configuration directory is poorly formed.[/]");
+            Environment.Exit(1);
+        }
+        catch (Exception e) {
+            AnsiConsole.MarkupLine($"[red]Could not load configuration: {e.Message}[/]");
+            Environment.Exit(1);
+        }
+    }
+
+#endregion
 }
---
+++ Terminal/Services/FileConfigurationManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Terminal.Domain.Config;
+
+namespace Terminal.Services;
+
+public class FileConfigurationManager(string path)
+    : IConfigurationManager
+{
+#region Fields
+
+    private readonly JsonSerializerOptions _serializerOptions = new() {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true
+    };
+
+#endregion
+
+#region Inherited
+
+    /// <inheritdoc cref="IConfigurationManager.Load"/>
+    public Config Load() {
+        EnsureConfigDirectoryExists();
+        var json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<Config>(json, _serializerOptions)
+               ?? new Config();
+    }
+
+    /// <inheritdoc cref="IConfigurationManager.Save"/>
+    public void Save(Config config) {
+        ArgumentNullException.ThrowIfNull(config);
+        var json = JsonSerializer.Serialize(config, _serializerOptions);
+        File.WriteAllText(path, json);
+    }
+
+#endregion
+
+#region Internals
+
+    /// <summary>
+    /// Creates an empty JSON file if one does not exist.
+    /// </summary>
+    private void EnsureConfigDirectoryExists() {
+        if (File.Exists(path)) {
+            return;
+        }
+
+        var json = JsonSerializer.Serialize(new Config(), _serializerOptions);
+        File.WriteAllText(path, json);
+    }
+
+#endregion
+}
