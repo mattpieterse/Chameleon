6a1b3cb6c686e37a33bb44a9c3f24ff14eaaf14f
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-05-17 06:22:19 +0200

    feat(config): create validation utility to verify basic schema integrity

---
+++ Terminal/Validation/ConfigValidator.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Mail;
+using Terminal.Domain.Config;
+
+namespace Terminal.Validation;
+
+public class ConfigValidator
+    : IValidator<Config>
+{
+#region Inherited
+
+    public void Validate(Config instance) {
+        if (instance is null) {
+            throw new ArgumentNullException(nameof(instance));
+        }
+
+        if (instance.Uses.Count == 0) {
+            throw new InvalidOperationException("No git accounts have been configured.");       
+        }
+
+        // Validate that fields match the expected type and are not empty
+        for (var i = 0; i < instance.Uses.Count; i++) {
+            var item = instance.Uses[i];
+
+            if (string.IsNullOrWhiteSpace(item.Name)) {
+                throw new InvalidOperationException($"Account[{i}] {nameof(item.Name).ToLower()} cannot be empty.]");
+            }
+
+            if (string.IsNullOrWhiteSpace(item.Path)) {
+                throw new InvalidOperationException($"Account[{i}] {nameof(item.Path).ToLower()} cannot be empty.]");
+            }
+
+            if (string.IsNullOrWhiteSpace(item.Configs.Alias)) {
+                throw new InvalidOperationException(
+                    $"Account[{i}] {nameof(item.Configs.Alias).ToLower()} cannot be empty.]");
+            }
+
+            if (string.IsNullOrWhiteSpace(item.Configs.Email)) {
+                throw new InvalidOperationException(
+                    $"Account[{i}] {nameof(item.Configs.Email).ToLower()} cannot be empty.]");
+            }
+
+            // Validate email formatting
+            try {
+                _ = new MailAddress(item.Configs.Email);
+            }
+            catch (Exception e) {
+                throw new InvalidOperationException(
+                    $"Account[{i}] {nameof(item.Configs.Email).ToLower()} is not a valid email address.");
+            }
+        }
+    }
+
+#endregion
+}
---
+++ Terminal/Validation/IValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace Terminal.Validation;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T">
+/// The object to validate.
+/// </typeparam>
+public interface IValidator<in T>
+{
+#region Contract
+
+    void Validate(T instance);
+
+#endregion
+}
